364388b1e89baed7044a73fec6c1c5e60fd7ac02
Nella <nella.latta@gmail.com>
2025-02-11 14:58:50 -0800

    Add pause and resume

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool gamePaused = false;
+
+    [SerializeField] private GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+
+    private void Pause() // TODO Unallow player input while paused
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // Can use for slow motion effects later
+        gamePaused = true;
+    }
+}
